23a9f517f1f99626b919d1b528daa9ddba1d2207
André Hoffmann <antihero2006@gmail.com>
2015-11-30 09:39:01 +0100

    improved error handling

--- src/batch/rsjobeditor.cc
+++ src/batch/rsjobeditor.cc
@@ -1,12 +1,30 @@
+#include <QtGui>
 #include <QApplication>
 #include <QWidget>
 #include "jobeditor/rsjobeditorapplication.h"
 #include "rscommon.h"
 #include "utils/rsstring.h"
 
+class JobEditorApplication : public QApplication {
+    public:
+        JobEditorApplication(int &argc, char **argv) : QApplication(argc, argv)
+        { }
+
+        virtual ~JobEditorApplication() { }
+
+        virtual bool notify(QObject * receiver, QEvent * event) {
+            try {
+                return QApplication::notify(receiver, event);
+            } catch(std::exception& e) {
+                qCritical() << "Exception thrown:" << e.what();
+            }
+            return false;
+        }
+};
+
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
+    JobEditorApplication app(argc, argv);
     JobEditorWindow widget;
     if ( argc > 1 ) {
         widget.openJob(rsString(argv[1]));
